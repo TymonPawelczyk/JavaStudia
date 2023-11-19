cb6176611153336f263b08eb20c50e4a0e039dea
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2023-11-19 15:37:38 +0100

    Update

--- Zajecia4/src/Przeszukiwalne.java
+++
@@ -1,6 +0,0 @@
-public interface Przeszukiwalne {
-    boolean czyPasuje(String wzorzec);
-    // co to jest interface i co zawiera
-
-    // Interface to klasa abstrakcyjna, ktorej uzywa sie do grupowania metod klasy z pustym cialem
-}
